97edbfb2dd3c35ae0ad3daf2cbb6bcb949865b89
Zohar <zohar.s0s@gmail.com>
2024-07-15 17:27:31 +0300

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import router from "./routes/Router";
-import { RouterProvider } from "react-router-dom";
 
 function App() {
-  return (
-    <Provider store={store}>
-        <RouterProvider router={router} />
-    </Provider>
-  );
+  return <div>hello world</div>
 }
 
 export default App;
